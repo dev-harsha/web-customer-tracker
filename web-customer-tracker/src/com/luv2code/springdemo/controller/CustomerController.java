b9bdac3f5d20f98aef8fbb8c7a0c9a4ac458cf25
fist22 <hnamanie1@gmail.com>
2020-03-18 17:56:26 +0530

    Add "Delete Customer" functionality

--- web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/luv2code/springdemo/controller/CustomerController.java
@@ -66,6 +66,14 @@ public class CustomerController {
 		return "customer-form";
 	}
 	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int id) {
+		
+		customerService.deleteCustomer(id);
+		
+		return "redirect:/customer/list";
+	}
+		
 	
 }
   
